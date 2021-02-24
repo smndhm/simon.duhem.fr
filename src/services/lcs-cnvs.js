1d20359b9dccd3c7349979be7bd262b088467abc
Simon <simon.duhem@gmail.com>
2021-02-24 10:07:18 +0100

    Revert "feat: links underline (#5)"
    This reverts commit b985a0e7ec4495576449aad1e1f316f980db464b.

--- src/services/lcs-cnvs.js
+++ src/services/lcs-cnvs.js
@@ -1,84 +1,73 @@
 class LcsCnvs {
   constructor(window) {
-    // Generative
-    this.generative = {
-      vertices: {
-        limit: 50,
-        list: [],
-      },
-      theme: {
-        colors: ["#25CE7B", "#DA38B5", "#FDC741", "#01B3E3", "#FF6B01"],
-        blendingMode: "multiply",
-      },
-      around: 50,
-      polygons: [],
-      underline: [],
-      interval: null,
+    this.around = 50;
+    this.vertices = {
+      limit: 50,
+      list: [],
+      themes: [
+        { colors: ["#25CE7B", "#DA38B5", "#FDC741", "#01B3E3", "#FF6B01"] },
+        {
+          colors: [
+            "#f72585",
+            "#b5179e",
+            "#7209b7",
+            "#560bad",
+            "#480ca8",
+            "#3a0ca3",
+            "#3f37c9",
+            "#4361ee",
+            "#4895ef",
+            "#4cc9f0",
+          ],
+        },
+      ],
     };
+    this.polygons = [];
 
-    // Window
     this.window = window;
 
-    this.window.addEventListener("resize", () => {
-      this.setCanvasSize();
-    });
+    this.window.onresize = () => {
+      this.canvas.width = this.window.innerWidth;
+      this.canvas.height = this.window.innerHeight;
+    };
 
-    // Mouse mouve events
+    let interval;
+
     ["mousemove", "touchmove"].forEach((eventType) => {
       this.window.addEventListener(
         eventType,
         (event) => {
-          clearInterval(this.generative.interval);
+          clearInterval(interval);
           const vertex = {
             x: event.pageX || (event.touches && event.touches[0].pageX) || 0,
             y: event.pageY || (event.touches && event.touches[0].pageY) || 0,
           };
           this.addVertex(vertex);
-          this.generative.interval = setInterval(() => {
-            this.removeVertex();
+          interval = setInterval(() => {
+            this.addVertex(vertex);
           }, 10);
         },
         false
       );
     });
 
-    // Document
     this.document = this.window.document;
 
-    // Add event on links
     this.document.querySelectorAll("nav a").forEach((el) => {
-      ["mouseover", "focusin"].forEach((eventType) => {
-        el.addEventListener(eventType, (event) => {
-          this.underline(event.target);
-        });
+      el.addEventListener("mouseover", (event) => {
+        this.notHover = event.target.getBoundingClientRect();
       });
-      ["mouseleave", "blur"].forEach((eventType) => {
-        el.addEventListener(eventType, () => {
-          this.generative.underline = [];
-          this.clearCanvas();
-        });
+      el.addEventListener("mouseleave", () => {
+        delete this.notHover;
       });
     });
 
-    // Canvas
     this.canvas = this.document.createElement("canvas");
-    this.canvas.id = "lcs-cnvs";
-    this.setCanvasSize();
-
-    this.ctx = this.canvas.getContext("2d");
-
-    // Append canvas to body
-    this.document.addEventListener("DOMContentLoaded", () => {
-      this.document.body.append(this.canvas);
-    });
-  }
-
-  /**
-   * Set canvas size
-   */
-  setCanvasSize() {
     this.canvas.width = this.window.innerWidth;
     this.canvas.height = this.window.innerHeight;
+    this.canvas.id = "lcs-cnvs";
+
+    this.ctx = this.canvas.getContext("2d");
   }
 
   /**
@@ -101,40 +90,38 @@ class LcsCnvs {
     );
   }
 
-  /**
-   *
-   * @param {*} array
-   */
-  getRandomArrayValue(array) {
-    return array[Math.floor(Math.random() * array.length)];
-  }
-
-  /**
-   *
-   */
-  updatePolygones() {
-    this.generative.polygons = this.generative.polygons.filter((polygon) =>
-      polygon.vertices.some((pVertex) =>
-        this.generative.vertices.list.some(
-          (vVertex) => pVertex.x === vVertex.x && pVertex.y === vVertex.y
-        )
-      )
+  isHover(vertex) {
+    return (
+      this.notHover &&
+      vertex.x > this.notHover.x &&
+      vertex.x < this.notHover.x + this.notHover.width &&
+      vertex.y > this.notHover.y &&
+      vertex.y < this.notHover.y + this.notHover.height
     );
   }
 
   /**
    *
+   * @param {*} area
    */
-  clearCanvas() {
-    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-  }
-
-  /**
-   *
-   * @param {*} ms
-   */
-  sleep(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
+  getRandomVertex(position, distance) {
+    let vertex;
+    do {
+      vertex = {
+        x: this.getRandomNumberBetween(
+          position.x - distance,
+          position.x + distance
+        ),
+        y: this.getRandomNumberBetween(
+          position.y - distance,
+          position.y + distance
+        ),
+      };
+    } while (
+      this.getVerticesDistance(position, vertex) > distance ||
+      this.isHover(vertex)
+    );
+    return vertex;
   }
 
   /**
@@ -156,63 +143,10 @@ class LcsCnvs {
 
   /**
    *
-   * @param {number} delay
+   * @param {*} array
    */
-  async drawPolygons(delay = 0) {
-    const polygons = this.generative.polygons.concat(this.generative.underline);
-    // Clear canvas
-    this.clearCanvas();
-
-    // Set blending mode
-    if (this.generative.theme.blendingMode) {
-      this.ctx.globalCompositeOperation = this.generative.theme.blendingMode;
-    }
-
-    // Add polygone
-    for (const { vertices, color } of polygons) {
-      this.ctx.beginPath();
-      this.ctx.moveTo(vertices[0].x, vertices[0].y);
-      for (let i = 1; i < vertices.length; i++) {
-        this.ctx.lineTo(vertices[i].x, vertices[i].y);
-      }
-      this.ctx.closePath();
-      this.ctx.fillStyle = color;
-      this.ctx.strokeStyle = color;
-      this.ctx.fill();
-      this.ctx.stroke();
-
-      // Add delay
-      if (delay) {
-        await this.sleep(delay);
-      }
-    }
-
-    // Clear if polygon list is empty
-    if (!polygons.length) {
-      this.clearCanvas();
-    }
-  }
-
-  /**
-   *
-   * @param {object} position
-   * @param {number} distance
-   */
-  getRandomVertex(position, distance) {
-    let vertex;
-    do {
-      vertex = {
-        x: this.getRandomNumberBetween(
-          position.x - distance,
-          position.x + distance
-        ),
-        y: this.getRandomNumberBetween(
-          position.y - distance,
-          position.y + distance
-        ),
-      };
-    } while (this.getVerticesDistance(position, vertex) > distance);
-    return vertex;
+  getRandomArrayValue(array) {
+    return array[Math.floor(Math.random() * array.length)];
   }
 
   /**
@@ -225,84 +159,48 @@ class LcsCnvs {
         x: position.x,
         y: position.y,
       },
-      this.generative.around
+      this.around
     );
 
-    if (this.generative.vertices.list.length >= 2) {
+    if (this.vertices.list.length >= 2) {
       const closestVertices = this.getClosestVertices(
-        [...this.generative.vertices.list],
+        [...this.vertices.list],
         vertex,
         2
       );
-      this.generative.polygons.push({
+      this.polygons.push({
         vertices: [vertex].concat(closestVertices),
-        color: this.getRandomArrayValue(this.generative.theme.colors),
+        color: this.getRandomArrayValue(this.vertices.themes[0].colors),
       });
     }
 
-    this.generative.vertices.list.push(vertex);
-    this.generative.vertices.list = this.generative.vertices.list.slice(
-      -this.generative.vertices.limit
+    this.vertices.list.push(vertex);
+    this.vertices.list = this.vertices.list.slice(-this.vertices.limit);
+
+    this.polygons = this.polygons.filter((polygon) =>
+      polygon.vertices.some((pVertex) =>
+        this.vertices.list.some(
+          (vVertex) => pVertex.x === vVertex.x && pVertex.y === vVertex.y
+        )
+      )
     );
 
-    this.updatePolygones();
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-    this.drawPolygons();
-  }
-
-  /**
-   *
-   */
-  removeVertex() {
-    this.generative.vertices.list.shift();
-    this.updatePolygones();
-    this.drawPolygons();
-    if (!this.generative.polygons.length) {
-      clearInterval(this.generative.interval);
-    }
-  }
-
-  /**
-   *
-   * @param {*} element
-   */
-  async underline(element) {
-    this.generative.vertices.list = [];
-    this.generative.polygons = [];
-    // Init generative
-    let vertices = [];
-
-    // Get element position
-    const bcr = element.getBoundingClientRect();
-    const y = bcr.y + bcr.height;
-
-    // Set polygons positions
-    for (let pos = bcr.x; pos <= bcr.x + bcr.width; pos += 10) {
-      // Set vertex
-      const vertex = {
-        x: pos,
-        y: this.getRandomNumberBetween(y, y + 10),
-      };
-
-      // Create polygon
-      if (vertices.length >= 2) {
-        const closestVertices = this.getClosestVertices(
-          [...vertices],
-          vertex,
-          2
-        );
-        this.generative.underline.push({
-          vertices: [vertex].concat(closestVertices),
-          color: this.getRandomArrayValue(this.generative.theme.colors),
-        });
+    for (const { vertices, color } of this.polygons) {
+      this.ctx.beginPath();
+      this.ctx.moveTo(vertices[0].x, vertices[0].y);
+      for (let i = 1; i < vertices.length; i++) {
+        this.ctx.lineTo(vertices[i].x, vertices[i].y);
       }
-
-      // Add vertex to list
-      vertices.push(vertex);
+      this.ctx.closePath();
+      this.ctx.fillStyle = color;
+      this.ctx.strokeStyle = color;
+      this.ctx.fill();
+      this.ctx.stroke();
     }
 
-    // Draw
-    this.drawPolygons(20);
+    this.document.body.append(this.canvas);
   }
 }
 
